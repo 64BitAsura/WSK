d8906f254d3dd736a38ea255649ae98df824d5fa
Bryan Olson <hikumealan@hotmail.com>
2023-11-03 03:08:17 +0000

    WIP packages

---
+++ wsk-stencil/.tmp/files/wsk/packages/angular-workspace/projects/angular-library/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-library
+ */
+
+export * from './lib/angular-library.module';
+export { DIRECTIVES } from './lib/stencil-generated';
+export * from './lib/stencil-generated/components';
---
+++ wsk-stencil/.tmp/files/wsk/packages/stencil-library/stencil.config.ts
@@ -0,0 +1,46 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget } from '@stencil/vue-output-target';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  plugins: [
+    sass()
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      // generateTypeDeclarations: true,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      outputType: 'component',
+      directivesProxyFile: '../angular-workspace/projects/angular-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/angular-library/src/lib/stencil-generated/index.ts',
+    }),
+    reactOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../react-library/lib/components/stencil-generated/index.ts',
+    }),
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../vue-library/lib/components.ts',
+    }),
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
