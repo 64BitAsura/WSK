f151f40e9dff150cef6f9cbb4a4449836e7a3c8f
Bryan Olson <hikumealan@hotmail.com>
2023-12-01 19:36:59 -0500

    WIP React Vite config fix

--- wsk-react/.tmp/vite/vite.config.ts
+++ wsk-react/.tmp/vite/vite.config.ts
@@ -4,19 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      server: {
-        fs: {
-          allow: [
-            searchForWorkspaceRoot(process.cwd()),
-            "..",
-            "../..",
-            "../../..",
-            "/wsk-stencil/wsk-app/packages/",
-            "/Users/hikumealan/Code/WSK/wsk-stencil/wsk-app/packages/stencil-library/dist/",
-          ],
-        },
-      },
-    }),
+    react(),
   ],
+  server:{
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()),"..","../..", "../../.."]
+    }
+  }
 });
